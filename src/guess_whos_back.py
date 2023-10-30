ab5479396b3883c15a6e38a53cd23506b6aad16a
Ivan Dubrovin <ivan.dubrovin@proton.me>
2023-10-30 19:18:42 +0300

    Update the Guess Who's Back script

--- src/guess_whos_back.py
+++ src/guess_whos_back.py
@@ -1,7 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-Xsub = np.load("data/raw/terrance/terrence.npz")["arr_0"]
+XX = np.load("data/raw/terrance/terrence.npz")["arr_0"]
+
+two_hundred_and_one = XX.copy()
+two_hundred_and_one = two_hundred_and_one.reshape(-1)
+two_hundred_and_one[two_hundred_and_one != 201] = 0
+two_hundred_and_one = two_hundred_and_one.reshape(XX.shape)
+
 plt.figure(figsize=(12, 12))
-plt.imshow(Xsub, cmap="hot")
+plt.imshow(XX, cmap="hot")
+plt.show()
+
+plt.figure(figsize=(12, 12))
+plt.imshow(two_hundred_and_one, cmap="hot")
 plt.show()
